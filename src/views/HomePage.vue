e64c2364ba699afd07024bed5beb7c7585004e66
roomdada <dsieroger@gmail.com>
2023-02-08 22:51:35 +0000

    fix login page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,53 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      <div id="container" class='mx-auto'>
-            <p class="form-title text-orange-500 text-2xl">Connectez-vous pour accéder à votre compte</p>
-              <ion-toast :is-open="isOpenRef" @didDismiss="setOpen(false)" message="Hello World!" :duration="1500"></ion-toast>
-            <form @submit.prevent="submit">
-                <input v-model='form.email' type="email" placeholder="E-mail"/> <br/>
-                <input v-model='form.password' type="password" placeholder="Mot de passe"/><br/>
-                <button type="submit" v-if="! loading">Se connecter maintenant</button>
-            </form>
+      <div class="font-sans text-gray-900 antialiased">
+            <!-- Page Container -->
+    <div id="page-container" class="flex flex-col mx-auto w-full min-h-screen bg-gray-100">
+        <!-- Page Content -->
+        <div class="w-full h-screen  md:flex lg:flex xl:flex">
+            <div class="bg-white flex justify-center flex-col lg:w-5/12 xl:w-5/12 md:w-6/12 w-full shadow-lg">
+                <div class="flex font-bold justify-center mb-6 mt-2">
+                    <img class="h-auto w-1/3" src="/assets/logo-ciprel-vf.png" alt="Logo">
+                </div>
+                <div class="w-full text-center px-16">
+                    <form class="w-full bg-white rounded-lg" method="POST" action="" @submit.prevent='submit'>
+                        <h2 class="text-xl text-center text-gray-700 my-4 mb-8 px-18">
+                            Bienvenue, veuillez-vous connecter à votre compte
+                        </h2>
+                        <x-auth-validation-errors />
+                        <div class="px-2 pb-4">
+                            <div class="w-full mt-8 mb-4">
+                                <div class="flex items-center">
+                                    <input type="text" id="tk-pages-sign-in-email" placeholder="Entrez votre email"
+                                        name="email"
+                                        class="px-2 w-full border rounded py-2 bg-opacity-25 text-gray-600 focus:outline-none bg-gray-100"
+                                        required autofocus />
+                                </div>
+                            </div>
+
+                            <div class="w-full mt-4">
+                                <div class="flex items-center">
+                                    <input type="password" id="tk-pages-sign-in-password" name="password"
+                                        placeholder="Entrez votre mot de passe"
+                                        class="px-2 w-full border rounded py-2 bg-opacity-25 text-gray-600 focus:outline-none bg-gray-100"
+                                        required autocomplete="current-password" />
+                                </div>
+                            </div>
+                            <button type="submit"
+                                class="w-full text-secondary-content uppercase px-4 rounded py-3 font-medium bg-primary mt-4  focus:outline-none">
+                                Connectez-vous
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <img src="/assets/login_image.jpg" alt="background"
+                class="object-cover object-center h-screen w-full md:w-6/12 lg:w-7/12 xl:w-7/12">
+        </div>
+    </div>
       </div>
+      {{ loading }}
     </ion-content>
   </ion-page>
 </template>
@@ -17,7 +55,6 @@
 <script setup>
 import { IonContent,
         IonPage,
-        IonToast
 } from '@ionic/vue';
 
 import { ref } from 'vue';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   future: {
     // removeDeprecatedGapUtilities: true,
@@ -12,7 +14,93 @@ module.exports = {
     fontFamily: {
       sans: ['Inter', ...defaultTheme.fontFamily.sans],
     },
+
+    colors : {
+      		primary: {
+					50: '#fef2e6',
+					100: '#fce5cc',
+					200: '#fbd8b3',
+					300: '#f9cb99',
+					400: '#f8be80',
+					500: '#f6b166',
+					600: '#f5a44d',
+					700: '#f39733',
+					800: '#f28a1a',
+					900: '#f07d00'
+				},
+				secondary: {
+					50: '#eef6ec',
+					100: '#dceed8',
+					200: '#cbe5c5',
+					300: '#b9ddb1',
+					400: '#a8d49e',
+					500: '#96cb8a',
+					600: '#85c377',
+					700: '#73ba63',
+					800: '#62b250',
+					900: '#50a93c'
+				},
+				accent: {
+					50: '#e9ecf1',
+					100: '#d2d9e3',
+					200: '#bcc7d4',
+					300: '#a5b4c6',
+					400: '#8fa1b8',
+					500: '#788eaa',
+					600: '#627b9c',
+					700: '#4b698d',
+					800: '#35567f',
+					900: '#1e4371'
+				},
+				grey: {
+					50: '#f7f7f8',
+					100: '#eff0f0',
+					200: '#e8e8e9',
+					300: '#e0e1e1',
+					400: '#d8d9da',
+					500: '#d0d1d2',
+					600: '#c8cacb',
+					700: '#c1c2c3',
+					800: '#b9bbbc',
+					900: '#b1b3b4'
+				},
+				danger: colors.rose,
+				success: colors.emerald,
+				warning: colors.amber
+    }
+
   },
   variants: {},
-  plugins: [],
+  daisyui: {
+		styled: true,
+		rtl: false,
+		logs: false,
+		themes: [
+			{
+				default: {
+					primary: '#f07d00',
+					'primary-focus': '#f07d00',
+					'primary-content': '#ffffff',
+					secondary: '#62b250',
+					'secondary-focus': '#50a93c',
+					'secondary-content': '#ffffff',
+					accent: '#35567f',
+					'accent-focus': '#1e4371',
+					'accent-content': '#ffffff',
+					neutral: '#bbbbbb',
+					'neutral-focus': '#b1b3b7',
+					'neutral-content': colors.gray[50],
+					'base-100': '#ffffff',
+					'base-200': '#f9fafb',
+					'base-300': '#d1d5db',
+					'base-content': '#1f2937',
+					info: '#2094f3',
+					success: '#009485',
+					warning: '#ff9900',
+					error: '#ff5724'
+				}
+			}
+		]
+	},
+	plugins: [require('@tailwindcss/typography'), require('daisyui'), require('@tailwindcss/custom-forms')]
 };
